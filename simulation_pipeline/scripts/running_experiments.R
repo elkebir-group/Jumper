2b38f709798e491704cf1b3d7c7405048bc2c8b7
Palash Sashittal <43966126+sashitt2@users.noreply.github.com>
2020-11-06 15:24:13 -0600

    removed unnecessary comments

--- simulation_pipeline/scripts/running_experiments.R
+++ simulation_pipeline/scripts/running_experiments.R
@@ -4,7 +4,6 @@ if(!require(polyester)) BiocManager::install("polyester")
 library(polyester)
 library(Biostrings)
 
-#fasta_file <- '../../simulation_pipeline/transcripts/neg_0.fasta'
 fasta_file <- paste('', snakemake@input[["transcriptFasta"]], sep = '')
 
 df <- read.csv(paste('', snakemake@input[["transcriptReadCounts"]], sep = ''), header = FALSE)
@@ -13,19 +12,3 @@ read_counts <- matrix(rep(df[[1]], each = snakemake@params[["nreplicates"]]), nc
 
 
 simulate_experiment_countmat(fasta_file, readmat=read_counts, outdir=paste('', snakemake@output[["outDir"]], sep = ''))
-
-#print(snakemake@input[[1]])
-
-#fasta_file <- paste('../../simulation_pipeline', snakemake@input[["transcriptFasta"]], sep = '/')
-#
-#df <- read.csv(paste('../../simulation_pipeline', 
-#                     snakemake@input[["transcriptReadCounts"]], sep = '/'), 
-#               header = FALSE)
-#
-#read_counts <- matrix(rep(df[[1]], each = snakemake@params[["nreplicates"]]), 
-#                      ncol = snakemake@params[["nreplicates"]], byrow = TRUE)
-#
-#
-#simulate_experiment_countmat(fasta_file, readmat=read_counts, 
-#                             outdir=paste('../../simulation_pipeline', 
-#                                          snakemake@output[["outDir"]], sep = '/'))
